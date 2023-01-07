1fab1e32471b76a903dbb67f4bfcf4e85f7f5235
unknown <iliaamm148@gmail.com>
2023-01-07 20:55:39 +0330

    set up the fron-end files

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 # Create your views here.
 
 
-
 def post_list(request):
-    post=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {"posts":post})
+    post = Post.objects.order_by("-published_date")
+    return render(request, 'blog/post_list.html', {"posts": post})
